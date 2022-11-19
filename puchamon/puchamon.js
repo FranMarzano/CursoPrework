55c76815736ff6e4c1f3d8e2869e52efb01e6d7c
FranMarzano <marzafran13@gmail.com>
2022-11-19 13:23:40 -0300

    Commit

---
+++ puchamon/puchamon.js
@@ -0,0 +1,6 @@
+function seleccionPuchamonJugador () {
+    alert(ELEGISTE TU PUCHAMON)
+}
+
+let botonPuchamonJugador = document.getElementById("boton-puchamon")
+botonPuchamonJugador.addEventListener("click", seleccionPuchamonJugador)
